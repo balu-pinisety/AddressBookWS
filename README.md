cde653c5160bb4b2792dff437172a87fb8941069
balu-pinisety <manikantapandu225@gmail.com>
2021-07-31 09:01:25 +0530

    Added: File for Address Book Program

---
+++ README.md
@@ -0,0 +1,4 @@
+# Address Book
+* Program for the Creation of Address Book Program
+* Ability to add details of Multiple persons
+* Ability to search and edit the details
---
+++ src/main/java/com/AddressBook/AddressBookService.java
@@ -0,0 +1,10 @@
+package com.AddressBook;
+
+public class AddressBookService {
+	
+	public static void main(String[] args) {
+	// Displaying Welcome Message
+	System.out.println("Welcome to Address Book Program");
+	}
+
+}
